89e4bd3f200368eda3b6a5822d5e180ece0654b6
Vishal Vaibhav <Vishal Vaibhav>
2024-04-30 14:33:28 +0530

    [Vishal,Tamiliniyan] code for unit conversion test equality

--- src/main/java/com/example/comparison/Scale.java
+++ src/main/java/com/example/comparison/Scale.java
@@ -5,23 +5,44 @@ import java.util.Objects;
 public class Scale {
 
     int value;
+    String unit;
 
-    public Scale(int value) {
+    public Scale(int value, String unit) {
 
         if(value <=0)
             throw new IllegalArgumentException();
 
         this.value = value;
+        this.unit = unit;
     }
-
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Scale scale = (Scale) o;
-        return value == scale.value;
+        return value == scale.value && Objects.equals(unit, scale.unit);
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, unit);
+    }
 
+    public Scale selfAdittion(Scale scale){
+        return new Scale(scale.value+ scale.value,scale.unit );
+    }
+
+    static Boolean conversionIsEqual(Scale measurement1 , Scale measurement2){
+
+       if(measurement1.unit.equals("cm")){
+
+           return ((measurement2.value * 100) == measurement1.value);
+
+       }
+
+       else
+           return ((measurement1.value * 100) == measurement2.value);
+
+   }
 }
--- src/test/java/com/example/comparison/ScaleTest.java
+++ src/test/java/com/example/comparison/ScaleTest.java
@@ -10,8 +10,8 @@ class ScaleTest {
     @Test
      void bothValuesAreEqual() {
 
-        Scale scaleLhs = new Scale(1);
-        Scale scaleRhs = new Scale(1);
+        Scale scaleLhs = new Scale(1,"cm");
+        Scale scaleRhs = new Scale(1,"cm");
 
         assertEquals(scaleLhs,scaleRhs);
 
@@ -20,8 +20,8 @@ class ScaleTest {
     @Test
     void bothValuesAreNotEqual() {
 
-        Scale scaleLhs = new Scale(1);
-        Scale scaleRhs = new Scale(2);
+        Scale scaleLhs = new Scale(1,"cm");
+        Scale scaleRhs = new Scale(2,"cm");
 
        assertNotEquals(scaleLhs,scaleRhs);
 
@@ -31,9 +31,51 @@ class ScaleTest {
     void invalidValues() {
 
         assertThrows(IllegalArgumentException.class,() -> {
-           new Scale(-1).equals(new Scale(-1));
+           new Scale(-1,"cm").equals(new Scale(-1,"cm"));
         });
 
+    }
+
+    @Test
+    void OnecmPlusOnecmEqualsTwocm(){
+
+        Scale measurement = new Scale(1,"cm");
+        Scale expectedMeasurement = new Scale(2,"cm");
+
+        Scale result = measurement.selfAdittion(measurement);
+
+        assertEquals(expectedMeasurement,result);
+
+    }
+
+
+    @Test
+    void OnecmPlusOnecmNotEqualsFourcm(){
+
+        Scale measurement = new Scale(1,"cm");
+        Scale expectedMeasurement = new Scale(4,"cm");
+
+        Scale result = measurement.selfAdittion(measurement);
+
+        assertNotEquals(expectedMeasurement,result);
+
+    }
+
+    @Test
+    void OneMeterEqualsHundredCm(){
+        Scale measurement1 = new Scale(1,"m");
+        Scale measurement2 = new Scale(100,"cm");
+
+        assertTrue(Scale.conversionIsEqual(measurement1,measurement2));
+    }
+
+
+    @Test
+    void HundredCmEqualsOneMeter(){
+        Scale measurement1 = new Scale(100,"cm");
+        Scale measurement2 = new Scale(1,"m");
+
+        assertTrue(Scale.conversionIsEqual(measurement1,measurement2));
     }
 
 
